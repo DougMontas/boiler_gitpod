bead469e037f6570e7db1f7fbdfb032e2e0108a1
DougMontas <dougmontas@gmail.com>
2022-10-21 09:49:45 +0000

    added goals routes and controllers

---
+++ mern-app/backend/controllers/goalController.js
@@ -0,0 +1,30 @@
+const asyncHandler = require('express-async-handler')
+
+
+// GET 
+const getGoals = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Get Goals' })
+})
+// POST
+const setGoal = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+   }
+    res.status(200).json({ message: 'Set Goal' })
+})
+// PUT
+const updateGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Update Goal ${req.params.id}` })
+})
+//DELETE
+const deleteGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Delete Goal ${req.params.id}` })
+})
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+}
