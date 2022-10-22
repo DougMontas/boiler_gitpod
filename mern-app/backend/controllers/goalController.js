fd495f004966eaa639b524cc93ddfb1829fdeb37
DougMontas <dougmontas@gmail.com>
2022-10-22 07:28:02 +0000

    REST API for Goals

--- mern-app/backend/controllers/goalController.js
+++ mern-app/backend/controllers/goalController.js
@@ -1,25 +1,43 @@
 const asyncHandler = require('express-async-handler')
-
+const Goal = require('../models/goalModel')
 
 // GET 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get Goals' })
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 // POST
 const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
-   }
-    res.status(200).json({ message: 'Set Goal' })
+    }
+    const goal = await Goal.create({
+        text: req.body.text
+    }) 
+    res.status(200).json(goal)
 })
 // PUT
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update Goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    
+    res.status(200).json(updatedGoal)
 })
 //DELETE
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete Goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error(`Goal not found`)
+    }
+    await Goal.findByIdAndDelete(req.params.id)
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
